09dfdd52055ccbedfb433e088468e285cef65699
CHKIM <ccmajkim1@gmail.com>
2020-05-23 08:40:12 -0700

    Added some furry changes

--- dog_years.cpp
+++ dog_years.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 
 int main() {
-  //Input your own dogs age after the = the example age is set to 6.
-  int dog_age = 6;
-  dog_age = dog_age;
+    //Input your own dogs age after the = the example age is set to 6.
+    int dog_age = 10;
+    dog_age = dog_age;
 
-  int early_years, later_years, human_years;
+    int early_years, later_years, human_years;
 
-  //The first two years of a dog's life count as 21 human years.
-  early_years = 21;
-  
-  //Each following years couts as 4 human years.
-  later_years = (dog_age - 2) * 4;
-  
-  //Adding Early and Later years, while storing inside the var Human
-  human_years = early_years + later_years;
-  
-  //You can change your dogs name too! Just replace "Chewy"!
-  std::cout << "My name is Chewy! " << "I am " << human_years << "years old in human years.\n"; 
- 
+    //The first two years of a dog's life count as 21 human years.
+    early_years = 21;
+
+    //Each following years couts as 4 human years.
+    later_years = (dog_age - 2) * 4;
+
+    //Adding Early and Later years, while storing inside the var Human
+    human_years = early_years + later_years;
+
+    //You can change your dogs name too! Just replace "Chewy"!
+    std::cout << "My name is Aldo! " << "I am " << human_years << " dog years young. WOOF\n";
+
 }
